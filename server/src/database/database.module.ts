dd66f592d4ceec7ac846fe9d12408aed25596950
Artur Shevchenko <shevchenko-a@bubuntu-lx.nixsolutions.com>
2022-01-12 18:58:05 +0200

    created database module

---
+++ server/src/database/database.module.ts
@@ -0,0 +1,24 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
+@Module({
+  imports: [
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => {
+        const username = configService.get('MONGO_USERNAME');
+        const password = configService.get('MONGO_PASSWORD');
+        const database = configService.get('MONGO_DATABASE');
+        const host = configService.get('MONGO_HOST');
+
+        return {
+          uri: `mongodb://${username}:${password}@${host}`,
+          dbName: database,
+        };
+      },
+      inject: [ConfigService],
+    }),
+  ],
+})
+export class DatabaseModule {}
