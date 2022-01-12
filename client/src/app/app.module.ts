4ac4f07fef54a48054a0f25011461005f4adefe3
Artur Shevchenko <shevchenko-a@bubuntu-lx.nixsolutions.com>
2022-01-12 16:01:57 +0200

    created play-zone cmpnt

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import {SettingsComponent} from "./settings/settings.component";
 import { MainComponent } from './main/main.component';
 import { AchievementsComponent } from './achievements/achievements.component';
+import { PlayZoneComponent } from './play-zone/play-zone.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SettingsComponent,
     MainComponent,
-    AchievementsComponent
+    AchievementsComponent,
+    PlayZoneComponent
   ],
   imports: [
     BrowserModule,
--- client/src/app/routes.ts
+++ client/src/app/routes.ts
@@ -2,9 +2,11 @@ import {Routes} from "@angular/router";
 import {SettingsComponent} from "./settings/settings.component";
 import {MainComponent} from "./main/main.component";
 import {AchievementsComponent} from "./achievements/achievements.component";
+import {PlayZoneComponent} from "./play-zone/play-zone.component";
 
 export const routes: Routes = [
   { path: '', component: MainComponent },
   { path: 'settings', component: SettingsComponent },
   { path: 'achievements', component: AchievementsComponent },
+  { path: 'play-zone', component: PlayZoneComponent },
 ];
