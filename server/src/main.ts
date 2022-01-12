2d455303f12cc5d66187fa1f20aaed1f3f9a0f92
Artur Shevchenko <shevchenko-a@bubuntu-lx.nixsolutions.com>
2022-01-12 17:03:08 +0200

    setup prod and dev environments

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import {Logger} from "@nestjs/common";
 
 async function bootstrap() {
+  const logger = new Logger('SERVER')
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const PORT = process.env.PORT;
+  await app.listen(PORT, () => logger.log(`Server started on ${PORT} port`));
 }
 bootstrap();
